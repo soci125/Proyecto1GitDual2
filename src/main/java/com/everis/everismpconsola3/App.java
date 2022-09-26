7fb40fa27b0bc70a0fd329287b00a987bc3111c8
VictorLlamasAmarillo <114400810+VictorLlamasAmarillo@users.noreply.github.com>
2022-09-26 14:27:24 +0200

    Update App.java

--- src/main/java/com/everis/everismpconsola3/App.java
+++ src/main/java/com/everis/everismpconsola3/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!!!!!    " );
+        System.out.println( "Hi, I'm Víctor" );        
         System.out.println( "GoodBye!!!!!    " );
     }
 }
