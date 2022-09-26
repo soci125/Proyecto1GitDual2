0558a5ba18067cba8edeed07fd9444c62f9a6daa
Fran Ferreras <franferreraszl@gmail.com>
2022-09-26 14:30:48 +0200

    Restablecer inicial

--- src/main/java/com/everis/everismpconsola3/App.java
+++ src/main/java/com/everis/everismpconsola3/App.java
@@ -10,6 +10,5 @@ public class App
     {
         System.out.println( "Hello World!!!!!    " );
         System.out.println( "GoodBye!!!!!    " );
-        System.out.println("Hola soy Fran Ferreras");
     }
 }
